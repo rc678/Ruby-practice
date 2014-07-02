90f612b025e7e147bf9e8b66372ccab8e92dd66d
rchaudhry <rosheenmail@gmail.com>
2014-07-02 14:12:47 -0400

    included basic error handling and fixed whitespace problem.

--- cipher/cipher.rb
+++ cipher/cipher.rb
@@ -5,7 +5,7 @@ Program will shift letters in string
 based on the number the user enters
 =end
 
-puts "Enter the sentence you want encrypted"
+puts "Enter the sentence you want encrypted."
 sentence = gets.chomp
 
 puts "Enter a number to shift"
@@ -14,10 +14,20 @@ number = number.to_i
 
 #Takes string and shifts letters based on input number
 def caesar_cipher(sentence, number)
-    sentence = sentence.split('').map { |ch| (ch.ord + number).chr}
+    sentence = sentence.split('').map { |ch|
+        if ch != ' ' then
+            (ch.ord + number).chr
+        else
+            ch.chr
+        end
+        }
+ 
     sentence = sentence.join
     puts sentence 
 end
 
-caesar_cipher(sentence, number)
-
+if(number >= 0 && number <= 26) then
+    caesar_cipher(sentence, number)
+else
+    puts "Number must be between 0 and 26"
+end
