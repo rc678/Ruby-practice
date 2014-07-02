422ea95093b7e3637755c7719e5985c2d5626d15
rchaudhry <rosheenmail@gmail.com>
2014-07-02 13:47:46 -0400

    completed simple grocery list program

--- array/grocery_list.rb
+++ array/grocery_list.rb
@@ -1,7 +1,23 @@
 =begin
 Practice with arrays
 -------------------------------------
-Program that makes to-do-list
+Program that makes a grocery list
 Will constantly ask for items until 
 sentinel value is entered
 =end
+
+user_input = ""
+list = Array.new()
+
+#adds item to array
+while user_input != "+" do
+puts "Enter what you need to do today. Exit by pressing +."
+user_input = gets.chomp
+list.push(user_input)
+end
+
+#removes + and prints list
+puts "Your Grocery list"
+puts "-----------------" 
+list.pop
+puts list
