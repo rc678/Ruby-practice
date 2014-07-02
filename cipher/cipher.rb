842d105c1afb12ea6946dbde972cd2ce81b3a9d4
rchaudhry <rosheenmail@gmail.com>
2014-07-02 00:18:18 -0400

    starting simple Caesar's cipher

---
+++ cipher/cipher.rb
@@ -0,0 +1,14 @@
+=begin
+Caesar's cipher algorithm
+Program will encrypt based on the
+number the user enters
+=end
+
+puts "Enter the sentence you want encrypted"
+sentence = gets.chomp
+
+puts "Enter a number to shift"
+number = gets.chomp
+
+
+
