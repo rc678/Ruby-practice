bb234b7b3b0200b8dd74976ac71077a6cd1ea811
rchaudhry <rosheenmail@gmail.com>
2014-07-08 22:58:07 -0400

    need to fix bubble_sort

--- bubble-sort/bubble.rb
+++ bubble-sort/bubble.rb
@@ -1,5 +1,28 @@
 #Bubble sort implementation
 
-def bubble_sort
-
+#takes in an unsorted array and returns a sorted array
+def bubble_sort(arr)
+    n = arr.length 
+    i = 1
+    j = 0
+    for i in i..n do
+        for j in j..n do
+            x = arr[j]
+            y = arr[j + 1]
+            if x > y then
+                arr[j] = arr[j+1]
+                arr[j + 1] = x
+                print arr
+                puts ""
+            end
+        end
+    end
+    #puts arr
+    #return arr
 end
+
+#testcases
+bubble_sort([4,25,99,2,0,2]) 
+#bubble_sort([1,2,3])
+#bubble_sort([3,2,1])
+
