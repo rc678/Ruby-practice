4570bf6476522fb7810e82d316ffce6ebb68baa3
rchaudhry <rosheenmail@gmail.com>
2014-07-13 19:57:31 -0400

    finished bubblesort implementation. works with all test cases.

--- bubble-sort/bubble.rb
+++ bubble-sort/bubble.rb
@@ -1,28 +1,31 @@
 #Bubble sort implementation
 
 #takes in an unsorted array and returns a sorted array
+#using the bubble sort algorithm
 def bubble_sort(arr)
     n = arr.length 
-    i = 1
-    j = 0
+    i = 0
+    j = 1
     for i in i..n do
-        for j in j..n do
-            x = arr[j]
-            y = arr[j + 1]
+        for j in j..(n-1) do
+            x = arr[j-1]
+            y = arr[j]
+            #swaps when j is less than j-1
             if x > y then
-                arr[j] = arr[j+1]
-                arr[j + 1] = x
-                print arr
-                puts ""
+                arr[j-1] = y
+                arr[j] = x 
             end
         end
+    #sets inner for loop value to 1
+    j = 1
     end
-    #puts arr
+    print arr
+    puts ""
     #return arr
 end
 
 #testcases
-bubble_sort([4,25,99,2,0,2]) 
-#bubble_sort([1,2,3])
-#bubble_sort([3,2,1])
+bubble_sort([5,1,4,2,8]) 
+bubble_sort([1,2,3])
+bubble_sort([3,2,1])
 
