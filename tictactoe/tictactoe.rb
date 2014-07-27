a9aa1690a5f84860889ca95457b5eb526a7f9bd7
rchaudhry <rosheenmail@gmail.com>
2014-07-27 02:05:52 -0400

    started filling out skeleton for class functions

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -10,17 +10,19 @@ puts "__|___|___"
 puts "4 | 5 | 6 "
 puts "__|___|___"
 puts "7 | 8 | 9 "
-puts "Each player must pick a number from 1 to 9"
-puts "The player who gets 3 places in a row wins the game"
+puts "Each player must pick a number from 1 to 9 representing the above board"
+puts "The player who gets 3 places in a row wins the game, including diagonals"
+puts "" 
 
 #initizes the Tic-Tac-Toe board
-board = {}
-9.times {|n| board[n+1] = "none"}
+$board = {}
+9.times {|n| $board[n+1] = "none"}
 
 #keeps track of the progress of the game
 class GameState
+    attr_accessor :state
     @@turns = 0    
-
+     
     def initialize(state)
       @state = state
     end
@@ -37,7 +39,7 @@ class GameState
     #finds out which player wins after no more turns are possible 
     private 
     def find_winner
-         
+             
     end
 
     #checks if any player won horizontally on the board
@@ -66,19 +68,54 @@ class GameState
 end
 
 class Player
-   @@nums = []
-    def intialize(name, symbol)
+    attr_reader :name
+    attr_reader :symbol 
+    @@nums = []
+    
+    def initialize(name, symbol)
       @name = name
       @symbol = symbol
     end
  
     #keeps track of players position on board
-    private
-    def place_in_board(position)
-
+    public
+    def place_in_board(position, psymbol)
+        if $board[position] == "none" then
+            $board[position] = psymbol
+            puts "Entered #{position} in board"
+        else
+            #COME BACK TO THIS. ERROR CHECKING NEEDED.
+            puts "#{position} already in use. Pick a different number."
+        end
     end   
 end 
 
+def begin_game
+    start_game = GameState.new(true)
+    p1_turn = true
+    puts "Enter name of Player 1. Player 1 will be X"
+    player_name = gets.chomp
+    p1 = Player.new(player_name, "X")
+    
+    puts "Enter name of Player 2. Player 2 will be O"
+    player_name = gets.chomp
+    p2 = Player.new(player_name, "O")
 
+    #asks player the moves they should make
+    while start_game.state
+        if p1_turn
+            puts "#{p1.name} Pick a number from 1-9 on the board"
+            num_on_board = gets.chomp.to_i
+            p1.place_in_board(num_on_board, p1.symbol)
+            p1_turn = false 
+        else
+            puts "#{p2.name} Pick a number from 1-9 on the board"
+            num_on_board = gets.chomp.to_i
+            p2.place_in_board(num_on_board, p2.symbol)
+            p1_turn = true
+        end
+    end
+end
 
+begin_game
 
