a2c468991cb30085b335aba9839d65d5b832d6bf
rchaudhry <rosheenmail@gmail.com>
2014-07-26 23:47:44 -0400

    initial commit

---
+++ tictactoe/tictactoe.rb
@@ -0,0 +1,18 @@
+=begin
+This is a command line version
+of the game Tic-Tac-Toe with
+two players
+=end
+
+puts "1 | 2 | 3 "
+puts "__|___|___"
+puts "4 | 5 | 6 "
+puts "__|___|___"
+puts "7 | 8 | 9 "
+puts "Each player must pick a number from 1 to 9"
+puts "The player who gets 3 places in a row wins the game"
+
+
+
+
+
